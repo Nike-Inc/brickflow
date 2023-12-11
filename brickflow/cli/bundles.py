2f1c3d4b27a9504df928b7b58798312bb513a313
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2023-12-11 13:55:59 -0500

    updated cli to 210.2 and added tests for the force flag

--- brickflow/cli/bundles.py
+++ brickflow/cli/bundles.py
@@ -223,7 +223,7 @@ def bundle_synth(**kwargs: Any) -> None:
 
 
 def get_bundle_cli_version() -> str:
-    return config(BrickflowEnvVars.BRICKFLOW_BUNDLE_CLI_VERSION.value, "0.207.0")
+    return config(BrickflowEnvVars.BRICKFLOW_BUNDLE_CLI_VERSION.value, "0.210.2")
 
 
 def bundle_cli_setup() -> None:
