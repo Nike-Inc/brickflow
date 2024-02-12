fc028df73febafe63be4c3535bb429b787f8a8ad
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2024-02-12 14:04:03 -0500

    added better error tracking with a custom error message indicating that it is a user code error, rather than framework error.

--- brickflow/engine/hooks.py
+++ brickflow/engine/hooks.py
@@ -13,6 +13,20 @@ brickflow_plugin_spec = pluggy.HookspecMarker(BRICKFLOW_TASK_PLUGINS)
 
 class BrickflowTaskPluginSpec:
     @staticmethod
+    def handle_user_result_errors(resp: "TaskResponse") -> None:
+        """Custom execute method that is able to be plugged in."""
+        if resp.user_code_error is not None:
+            original_message = str(resp.user_code_error)
+            additional_info = (
+                "BRICKFLOW_USER_OR_DBR_ERROR: This is an error thrown in user code. \n"
+                f"BRICKFLOW_INPUT_ARGS: {resp.input_kwargs}\n"
+                "Original Exception Message: "
+            )
+            new_message = additional_info + original_message
+            resp.user_code_error.args = (new_message,)
+            raise resp.user_code_error
+
+    @staticmethod
     @brickflow_plugin_spec(firstresult=True)
     def task_execute(task: "Task", workflow: "Workflow") -> "TaskResponse":
         """Custom execute method that is able to be plugged in."""
---
+++ tests/resolver/test_resolver.py
@@ -0,0 +1,52 @@
+# test_resolver.py
+from typing import Type
+
+import pytest
+
+import brickflow
+from brickflow.resolver import (
+    BrickflowRootNotFound,
+)
+
+
+@pytest.fixture
+def default_mocks(mocker):
+    # Create mocks for the three methods
+    mocker.patch(
+        "brickflow.resolver.get_caller_file_paths", return_value=["path1", "path2"]
+    )
+    mocker.patch(
+        "brickflow.resolver.get_notebook_ws_path", return_value="/notebook/ws/path"
+    )
+
+
+def test_resolver_methods(default_mocks, mocker):
+    error_msg = "This is a test message"
+
+    def make_exception_function(exc: Type[Exception]):
+        def raise_exception(*args, **kwargs):
+            raise exc(error_msg)
+
+        return raise_exception
+
+    # catch random error
+    mocker.patch(
+        "brickflow.resolver.go_up_till_brickflow_root",
+        side_effect=make_exception_function(ValueError),
+    )
+    with pytest.raises(ValueError, match=error_msg):
+        brickflow.resolver.get_relative_path_to_brickflow_root()
+
+    mocker.patch(
+        "brickflow.resolver.go_up_till_brickflow_root",
+        side_effect=make_exception_function(BrickflowRootNotFound),
+    )
+
+    brickflow.resolver.get_relative_path_to_brickflow_root()
+
+    mocker.patch(
+        "brickflow.resolver.go_up_till_brickflow_root",
+        side_effect=make_exception_function(PermissionError),
+    )
+
+    brickflow.resolver.get_relative_path_to_brickflow_root()
